6620b255b67397955b03f4d58141045c2edc97a8
DevonMartin <85790366+DevonMartin@users.noreply.github.com>
2022-09-14 15:46:37 -0400

    Finished lab1

--- fin/lab1/Collatz.java
+++ fin/lab1/Collatz.java
@@ -3,15 +3,11 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Corrected implementation of nextNumber
+     * which finds the next number based on whether
+     * the current number is even or odd.*/
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
+        return (n % 2 == 0) ? n / 2 : 3 * n + 1;
     }
 
     public static void main(String[] args) {
