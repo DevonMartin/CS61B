bfc3f5796799f181a9d2eedefd605c39f4088f25
Devon Martin <85790366+DevonMartin@users.noreply.github.com>
2022-10-03 10:15:56 -0400

    Update Collatz.java
    Implement buggy nextNumber to test merge conflicts for lab4

--- fin/lab1/Collatz.java
+++ fin/lab1/Collatz.java
@@ -3,11 +3,15 @@
  */
 public class Collatz {
 
-    /** Corrected implementation of nextNumber
-     * which finds the next number based on whether
-     * the current number is even or odd.*/
+    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        return (n % 2 == 0) ? n / 2 : 3 * n + 1;
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
+        }
     }
 
     public static void main(String[] args) {
